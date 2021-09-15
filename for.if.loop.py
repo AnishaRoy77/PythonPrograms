526dccd43f7c3d5e7d75b8a86250acea20924b15
Anisha Roy <76565991+AnishaRoy77@users.noreply.github.com>
2021-09-15 11:42:45 +0530

    Update for.if.loop.py

--- for.if.loop.py
+++ for.if.loop.py
@@ -1,4 +1,4 @@
-#to print numbers from 1 to 21 , print onlu numbers which i not divisible by 5
+#to print numbers from 1 to 21 , print only numbers which i not divisible by 5
 
 for i in range(1,21):
     if(i%5!=0):
